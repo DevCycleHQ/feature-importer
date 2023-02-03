0e8d86cdaa51409b0bddae83e9a5b5eb7be23255
Parth Suthar <parth.suthar@taplytics.com>
2023-02-03 17:47:31 -0500

    [chore] add configs file and config checking

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import { configs } from './configs'
+
+
+if (configs.ldAccessToken === '')
+    throw Error("ldAccessToken cannot be empty")
+if (configs.dvcClientId === '')
+    throw Error("dvcClientId cannot be empty")
+if (configs.dvcClientSecret === '')
+    throw Error("dvcClientSecret cannot be empty")
+if (configs.dvcProjectKey === '')
+    throw Error("dvcProjectKey cannot be empty")
+
+
+console.log(configs)
