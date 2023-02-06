b030e6f33492de4626457495bc3fe1ef2e1c7c5a
Parth Suthar <parth.suthar@taplytics.com>
2023-02-06 12:44:31 -0500

    [refactor] remove unecessary code

--- src/LDApiWrapper.ts
+++ src/LDApiWrapper.ts
@@ -1,15 +1,5 @@
 const LD_BASE_URL = "https://app.launchdarkly.com/api/v2";
 
-const getLDEnvironments = async (access_token: string, project_key: string) => {
-    const response = await fetch(`${LD_BASE_URL}/projects/${project_key}/environments`, {
-        method: "GET",
-        headers: {
-            Authorization: `${access_token}`,
-        },
-    });
-    const data = await response.json();
-    return data;
-};
 
 const getFeatureFlagsForProject = async (access_token: string, project_key: string,) => {
     const response = await fetch(`${LD_BASE_URL}/flags/${project_key}`, {
@@ -22,20 +12,4 @@ const getFeatureFlagsForProject = async (access_token: string, project_key: stri
     return data;
 }
 
-const getProjects = async (access_token: string) => {
-    const response = await fetch(
-        `${LD_BASE_URL}/projects`,
-        {
-            method: 'GET',
-            headers: {
-                Authorization: access_token
-            }
-        }
-    );
-
-    const data = await response.json();
-    return data;
-}
-
-
-export default { getLDEnvironments, getProjects, getFeatureFlagsForProject };
+export default { getFeatureFlagsForProject };
