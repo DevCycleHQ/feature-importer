1eb33c7dc6ae60b90036556fc05396146df356ec
Parth Suthar <parth.suthar@taplytics.com>
2023-02-06 11:49:50 -0500

    [refactor] add configs file as a JSON file or set configs through env vars

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,14 @@ import DVCWrapper from './DevcycleApiWrapper'
 import LDApiWrapper from './LDApiWrapper'
 
 const configs = getConfigs()
-
 if (configs.ldAccessToken === '')
     throw Error("ldAccessToken cannot be empty")
 if (configs.dvcClientId === '')
     throw Error("dvcClientId cannot be empty")
 if (configs.dvcClientSecret === '')
-    throw Error("dvcClientSecret cannot be empty")
+    throw Error('dvcClientSecret cannot be empty')
 if (configs.projectKey === '')
-    throw Error("projectKey cannot be empty")
+    throw Error('projectKey cannot be empty')
 
 let apiToken: string = '';
 
