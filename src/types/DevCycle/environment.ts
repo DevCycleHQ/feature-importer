788f48b8480bbbf7fd8ee8879e5c0642ee6ee76f
Joli Lui <joli.lui@taplytics.com>
2023-02-09 14:15:10 -0500

    [chore] separate types file for different resources

---
+++ src/types/DevCycle/environment.ts
@@ -0,0 +1,27 @@
+export type EnvironmentPayload = {
+    name: string
+    key: string
+    type: string
+    description?: string
+    color?: string
+}
+
+export type EnvironmentResponse = {
+    _id: string
+    _project: string
+    name: string
+    key: string
+    type: string
+    color: string
+    _createdBy: string
+    createdAt: string
+    updatedAt: string
+    sdkKeys: Record<string, unknown>
+}
+
+export enum EnvironmentType {
+    Dev = "development",
+    Staging = "staging",
+    Prod = "production",
+    Recovery = "disaster_recovery"
+}
